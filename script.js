02a0ddb9d2765fb735b20a0e1b813fe8281a7aa5
zfarmertic <lilyigbunu@gmail.com>
2025-12-03 22:47:16 +0100

    to do list

--- script.js
+++ script.js
@@ -29,8 +29,8 @@ document.addEventListener('DOMContentLoaded', ()=>{
 
     
     addButton.addEventListener('click', addTask)
-    taskInput.addEventListener("keypress", (e)=>{
-        if(e.key === 'Enter'){
+    taskInput.addEventListener("keypress", (event)=>{
+        if(event.key === 'Enter'){
             e.preventDefault();
 
             addTask()
